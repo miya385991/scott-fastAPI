6052db9d3f48567366c9edbe4f31f4396cd5e18b
yusuke <38mtyuusuuke@gmail.com>
2022-06-14 17:55:46 +0900

    PostgreSQL add

--- database.py
+++ database.py
@@ -1,8 +1,13 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./scottadmin.db"
+SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
+if SQLALCHEMY_DATABASE_URL.startswith('postgress://'):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://",
+                                                              "postgresql://",
+                                                              1)
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False}
@@ -11,5 +16,3 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
-
