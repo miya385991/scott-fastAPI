c6bad349c14d1f677bc06b8377de7e70de754b17
yusuke <38mtyuusuuke@gmail.com>
2022-06-14 18:54:04 +0900

    database name fix

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
-if SQLALCHEMY_DATABASE_URL.startswith('postgress://'):
+if SQLALCHEMY_DATABASE_URL.startswith('postgres://'):
     SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://",
                                                               "postgresql://",
                                                               1)
